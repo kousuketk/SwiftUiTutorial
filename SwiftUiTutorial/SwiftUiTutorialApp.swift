8c8905b4c51ae566730ccb9d5730404948f6d80f
kousuke takahashi <pamarikomi_t@icloud.com>
2022-05-26 22:09:24 +0900

    Initial Commit

---
+++ SwiftUiTutorial/SwiftUiTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTutorialApp.swift
+//  SwiftUiTutorial
+//
+//  Created by Takahashi on 2022/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
