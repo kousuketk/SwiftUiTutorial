741b60e05c15de688b02bc488f21adc22aa2353a
kousuke takahashi <pamarikomi_t@icloud.com>
2022-05-28 18:17:12 +0900

    modify Models

--- SwiftUiTutorial/Model/Landmark.swift
+++ SwiftUiTutorial/Model/Landmark.swift
@@ -7,20 +7,19 @@ struct Landmark: Hashable, Codable {
     var name: String
     var park: String
     var state: String
-    var description: String
-    
+
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-    
+
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
-            latitude: coordinates.latitude, longitude: coordinates.longitude
-        )
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
     }
-    
+
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
