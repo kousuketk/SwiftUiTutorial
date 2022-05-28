f9a118f24530f2ac4bfce28f47836c60d2c8ac42
kousuke takahashi <pamarikomi_t@icloud.com>
2022-05-28 19:44:34 +0900

    finished section4 of building-lists-and-navigation

---
+++ SwiftUiTutorial/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  SwiftUiTutorial
+//
+//  Created by Takahashi on 2022/05/28.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
